808176a6c077324f30eb1aa3c5142229a44731af
Marcello <mespositow@gmail.com>
2025-07-15 18:42:15 +0200

    implement topology slicing

--- src/ryu_controller.py
+++ src/ryu_controller.py
@@ -6,19 +6,30 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from common import *
 
 class RyuController(app_manager.RyuApp):
     OFP_VERSION = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(RyuController, self).__init__(*args, **kwargs)
-
-        # MAC address dei 4 host
-        self.h1 = "00:00:00:00:00:01"
-        self.h2 = "00:00:00:00:00:02"
-        self.h3 = "00:00:00:00:00:03"
-        self.h4 = "00:00:00:00:00:04"
-
+        self.cdn1 = mac_cdns[0]
+        self.cdn2 = mac_cdns[1]
+        
+        self.h1 = mac_hosts[0]
+        self.h2 = mac_hosts[1]
+        self.h3 = mac_hosts[2]
+        self.h4 = mac_hosts[3]
+        
+        self.out_port_map = {
+            1:  {self.cdn1: 1, self.cdn2: 2, 2: 3, 4: 4},
+            2:  {1: 1, 3: 2, 6: 3},
+            3:  {2: 1, 6: 2},
+            4:  {1: 1, 5: 2, 6: 3},
+            5:  {4: 1, 6: 2},
+            6:  {self.h1: 3, self.h2: 4, self.h3: 7, self.h4: 8, 3: 1, 2: 2, 5: 6, 4: 5}
+        }
+    
     # new switch connected event
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -27,43 +38,117 @@ class RyuController(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         dpid = datapath.id
 
-        # Rimuove il comportamento di default (flood)
+        # Disable default flooding
         match = parser.OFPMatch()
         actions = []
         self.add_flow(datapath, 0, match, actions)
 
-        # Regole statiche per ogni switch
         if dpid == 1:
-            # s1
-            # h1 -> s2
-            self.add_mac_flow(datapath, self.h1, self.h3, 2)
-            # h3 -> s1 -> h1
-            self.add_mac_flow(datapath, self.h3, self.h1, 1)
+            # --- Top slice handling (cdn1 <-> h1, h2) ---
+            port_out_s2 = self.get_out_port(dpid, 2)  # forward toward s2
+            self.add_mac_flow(datapath, self.cdn1, self.h1, port_out_s2)
+            self.add_mac_flow(datapath, self.cdn1, self.h2, port_out_s2)
+            self.add_arp(datapath, self.cdn1, port_out_s2)
+            
+            port_out_cdn1 = self.get_out_port(dpid, self.cdn1)  # reverse: toward cdn1
+            self.add_mac_flow(datapath, self.h1, self.cdn1, port_out_cdn1)
+            self.add_mac_flow(datapath, self.h2, self.cdn1, port_out_cdn1)
+            self.add_arp(datapath, self.h1, port_out_cdn1)
+            self.add_arp(datapath, self.h2, port_out_cdn1)
 
-            # h2 -> s3
-            self.add_mac_flow(datapath, self.h2, self.h4, 3)
-            # h4 -> s1 -> h2
-            self.add_mac_flow(datapath, self.h4, self.h2, 4)
+            # --- Bottom slice handling (cdn2 <-> h3, h4) ---
+            port_out_s4 = self.get_out_port(dpid, 4)  # forward toward s4
+            self.add_mac_flow(datapath, self.cdn2, self.h3, port_out_s4)
+            self.add_mac_flow(datapath, self.cdn2, self.h4, port_out_s4)
+            self.add_arp(datapath, self.cdn2, port_out_s4)
+            
+            port_out_cdn2 = self.get_out_port(dpid, self.cdn2)  # reverse: toward cdn2
+            self.add_mac_flow(datapath, self.h3, self.cdn2, port_out_cdn2)
+            self.add_mac_flow(datapath, self.h4, self.cdn2, port_out_cdn2)
+            self.add_arp(datapath, self.h3, port_out_cdn2)
+            self.add_arp(datapath, self.h4, port_out_cdn2)
 
+            
         elif dpid == 2:
-            # s2
-            self.add_mac_flow(datapath, self.h1, self.h3, 2)
-            self.add_mac_flow(datapath, self.h3, self.h1, 1)
-
+            # Traffic between cdn1 and hosts h1/h2
+            port_out_s3 = self.get_out_port(dpid, 3)
+            self.add_mac_flow(datapath, self.cdn1, self.h1, port_out_s3)
+            self.add_mac_flow(datapath, self.cdn1, self.h2, port_out_s3)
+            self.add_arp(datapath, self.cdn1, port_out_s3)
+            
+            port_out_s1 = self.get_out_port(dpid, 1) # reverse
+            self.add_mac_flow(datapath, self.h1, self.cdn1, port_out_s1)
+            self.add_mac_flow(datapath, self.h2, self.cdn1, port_out_s1)
+            self.add_arp(datapath, self.h1, port_out_s1)
+            self.add_arp(datapath, self.h2, port_out_s1)
+        
         elif dpid == 3:
-            # s3
-            self.add_mac_flow(datapath, self.h2, self.h4, 2)
-            self.add_mac_flow(datapath, self.h4, self.h2, 1)
+            # Traffic between cdn1 and hosts h1/h2
+            port_out_s6 = self.get_out_port(dpid, 6)
+            self.add_mac_flow(datapath, self.cdn1, self.h1, port_out_s6)
+            self.add_mac_flow(datapath, self.cdn1, self.h2, port_out_s6)
+            self.add_arp(datapath, self.cdn1, port_out_s6)
+            
+            port_out_s2 = self.get_out_port(dpid, 2) # reverse
+            self.add_mac_flow(datapath, self.h1, self.cdn1, port_out_s2)
+            self.add_mac_flow(datapath, self.h2, self.cdn1, port_out_s2)
+            self.add_arp(datapath, self.h1, port_out_s2)
+            self.add_arp(datapath, self.h2, port_out_s2)
 
         elif dpid == 4:
-            # s4
-            # path alto
-            self.add_mac_flow(datapath, self.h1, self.h3, 2)
-            self.add_mac_flow(datapath, self.h3, self.h1, 1)
-            # path basso
-            self.add_mac_flow(datapath, self.h2, self.h4, 3)
-            self.add_mac_flow(datapath, self.h4, self.h2, 4)
+            # Traffic between cdn2 and hosts h3/h4
+            port_out_s5 = self.get_out_port(dpid, 5)
+            self.add_mac_flow(datapath, self.cdn2, self.h3, port_out_s5)
+            self.add_mac_flow(datapath, self.cdn2, self.h4, port_out_s5)
+            self.add_arp(datapath, self.cdn2, port_out_s5)
+            
+            port_out_s1 = self.get_out_port(dpid, 1) # reverse
+            self.add_mac_flow(datapath, self.h3, self.cdn2, port_out_s1)
+            self.add_mac_flow(datapath, self.h4, self.cdn2, port_out_s1)
+            self.add_arp(datapath, self.h3, port_out_s1)
+            self.add_arp(datapath, self.h4, port_out_s1)
 
+        elif dpid == 5:
+            # Traffic between cdn2 and hosts h3/h4
+            port_out_s6 = self.get_out_port(dpid, 6)
+            self.add_mac_flow(datapath, self.cdn2, self.h3, port_out_s6)
+            self.add_mac_flow(datapath, self.cdn2, self.h4, port_out_s6)
+            self.add_arp(datapath, self.cdn2, port_out_s6)
+            
+            port_out_s4 = self.get_out_port(dpid, 4) # reverse
+            self.add_mac_flow(datapath, self.h3, self.cdn2, port_out_s4)
+            self.add_mac_flow(datapath, self.h4, self.cdn2, port_out_s4)
+            self.add_arp(datapath, self.h3, port_out_s4)
+            self.add_arp(datapath, self.h4, port_out_s4)
+
+        elif dpid == 6:  # s6: the common host switch
+            # --- Top slice handling (cdn1 <-> h1, h2) ---
+            port_out_h1 = self.get_out_port(dpid, self.h1)
+            port_out_h2 = self.get_out_port(dpid, self.h2)
+            self.add_mac_flow(datapath, self.cdn1, self.h1, port_out_h1)
+            self.add_mac_flow(datapath, self.cdn1, self.h2, port_out_h2)
+            self.add_arp(datapath, self.cdn1, port_out_h1)
+            
+            port_out_s3 = self.get_out_port(dpid, 3) # reverse
+            self.add_mac_flow(datapath, self.h1, self.cdn1, port_out_s3)
+            self.add_mac_flow(datapath, self.h2, self.cdn1, port_out_s3)
+            self.add_arp(datapath, self.h1, port_out_s3)
+            self.add_arp(datapath, self.h2, port_out_s3)
+            
+            # --- Bottom slice handling (cdn2 <-> h3, h4) ---
+            port_out_h3 = self.get_out_port(dpid, self.h3)
+            port_out_h4 = self.get_out_port(dpid, self.h4)
+            self.add_mac_flow(datapath, self.cdn2, self.h3, port_out_h3)
+            self.add_mac_flow(datapath, self.cdn2, self.h4, port_out_h4)
+            self.add_arp(datapath, self.cdn2, port_out_h3)
+            
+            port_out_s5 = self.get_out_port(dpid, 5) # reverse
+            self.add_mac_flow(datapath, self.h3, self.cdn2, port_out_s5)
+            self.add_mac_flow(datapath, self.h4, self.cdn2, port_out_s5)
+            self.add_arp(datapath, self.h3, port_out_s5)
+            self.add_arp(datapath, self.h4, port_out_s5)
+
+    
     def add_mac_flow(self, datapath, src, dst, out_port, priority=10):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -77,7 +162,16 @@ class RyuController(app_manager.RyuApp):
             match=match, instructions=inst
         )
         datapath.send_msg(mod)
+    
+    
+    def add_arp(self, datapath, eth_src, out_port):
+        parser = datapath.ofproto_parser
+        
+        match = parser.OFPMatch(eth_src=eth_src, eth_dst="ff:ff:ff:ff:ff:ff", eth_type=0x0806)
+        actions = [parser.OFPActionOutput(out_port)]
+        self.add_flow(datapath, priority=20, match=match, actions=actions)
 
+    
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -88,4 +182,24 @@ class RyuController(app_manager.RyuApp):
             match=match, instructions=inst
         )
         datapath.send_msg(mod)
+    
+    
+    def get_out_port(self, dpid_src, dest) -> int:
+        return self.out_port_map[dpid_src][dest]
+    
+    
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        dpid = datapath.id
+        in_port = msg.match['in_port']
+        
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        
+        src = eth.src
+        dst = eth.dst
+        
+        self.logger.info("PacketIn: dpid=%s src=%s dst=%s in_port=%s", dpid, src, dst, in_port)
         
---
+++ src/topology.py
@@ -0,0 +1,67 @@
+# Marcello gay
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from common import *
+
+class Environment(object):
+    def __init__(self):
+        self.net = Mininet(controller=RemoteController, switch=OVSKernelSwitch, link=TCLink)
+
+        self.c0 = self.net.addController('c0', controller=RemoteController)
+        self.c0.start()
+        
+        
+        self.cdn1 = self.net.addHost('cdn1', ip='10.0.0.1', mac=mac_cdns[0])
+        self.cdn2 = self.net.addHost('cdn2', ip='10.0.0.2', mac=mac_cdns[1])
+        
+        
+        self.h1 = self.net.addHost('h1', ip='10.0.0.4', mac=mac_hosts[0]) # premium user
+        self.h2 = self.net.addHost('h2', ip='10.0.0.5', mac=mac_hosts[1])
+        self.h3 = self.net.addHost('h3', ip='10.0.0.6', mac=mac_hosts[2])
+        self.h4 = self.net.addHost('h4', ip='10.0.0.7', mac=mac_hosts[3]) # premium user
+
+        self.s1 = self.net.addSwitch('s1') # cdns common switch
+        
+        self.s2 = self.net.addSwitch('s2') # top slice switches
+        self.s3 = self.net.addSwitch('s3')
+        
+        self.s4 = self.net.addSwitch('s4') # bottom slice switched
+        self.s5 = self.net.addSwitch('s5')
+        
+        self.s6 = self.net.addSwitch('s6') # hosts common switch
+        
+        # top slice: cdn1, s1, s2, s3, s6, h1, h2
+        self.net.addLink(self.cdn1, self.s1, bw=10, delay='1ms', port1=1, port2=1)
+        self.net.addLink(self.s1, self.s2, bw=10, delay='5ms', port1=3, port2=1)
+        self.net.addLink(self.s2, self.s3, bw=5, delay='10ms', port1=2, port2=1)
+        self.net.addLink(self.s3, self.s6, bw=5, delay='10ms', port1=2, port2=1)
+        self.net.addLink(self.s2, self.s6, bw=5, delay='3ms', port1=3, port2=2) # direct link for premium users
+        self.net.addLink(self.s6, self.h1, bw=8, delay='5ms', port1=3, port2=1) # s6 to h1, h2 links 
+        self.net.addLink(self.s6, self.h2, bw=8, delay='5ms', port1=4, port2=1) 
+
+        # bottom slice: cdn2, s1, s4, s5, s6, h3, h4
+        self.net.addLink(self.cdn2, self.s1, bw=10, delay='1ms', port1=1, port2=2)
+        self.net.addLink(self.s1, self.s4, bw=10, delay='5ms', port1=4, port2=1)
+        self.net.addLink(self.s4, self.s5, bw=5, delay='10ms', port1=2, port2=1)
+        self.net.addLink(self.s5, self.s6, bw=5, delay='10ms', port1=2, port2=6)
+        self.net.addLink(self.s4, self.s6, bw=5, delay='3ms', port1=3, port2=5) # direct link for premium users
+        self.net.addLink(self.s6, self.h3, bw=8, delay='5ms', port1=7, port2=1) # s6 to h3, h4 links 
+        self.net.addLink(self.s6, self.h4, bw=8, delay='5ms', port1=8, port2=1)
+
+        self.net.build()
+        self.net.start()
+
+        print("Topology started")
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    info('starting the environment\n')
+    env = Environment()
+
+    info("*** Running CLI\n")
+    CLI(env.net)
